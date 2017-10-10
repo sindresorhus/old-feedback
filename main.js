6e4683872a69ea76f4db580c0585326acef084e8
Sindre Sorhus <sindresorhus@gmail.com>
2017-10-10 15:06:55 +0700

    Various improvements

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 /* eslint-env browser, jquery */
-(function () {
-	'use strict';
-
-	// Super ugly, but who cares
-	// Intentionally using older JS syntax here as I want to ensure this works
+(() => {
 	if (window.location.search === '?success') {
 		$('#main').html('<h1 class="title is-2">Thanks for the feedback!</h1><p>You will now be redirected to my website.');
 
-		setTimeout(function () {
+		setTimeout(() => {
 			window.location.href = 'https://sindresorhus.com';
 		}, 3000);
 	}
-})();
 
-(() => {
-	'use strict';
+	// Borrow the navbar from the main site
+	$('#nav-container').load('https://sindresorhus.com/contact .hero-head', () => {
+		$('#nav-container').toggleClass('visible');
+	});
 
+	// Form validation
 	$('button[type="submit"]').on('click', event => {
 		$(event.target)
 			.closest('form')
@@ -26,25 +24,45 @@
 	const params = new URL(location.href).searchParams;
 
 	if (params.has('product')) {
-		$('#main h1 > span').text(` · ${params.get('product')}`);
+		const title = `Feedback for ${params.get('product')}`;
+		$('#main h1').text(title);
+		$('title').text(title);
 	}
 
 	const form = $('#feedback-form');
 
 	// Include all the existing search params
 	for (const [key, val] of params) {
-		if (key === 'message') {
+		if (key === 'nameField') {
+			form.find('[name="name"]').val(val);
+			continue;
+		}
+
+		if (key === 'emailField') {
+			form.find('[name="email"]').val(val);
+			continue;
+		}
+
+		if (key === 'messageField') {
 			form.find('[name="message"]').val(val);
 			continue;
 		}
 
-		form.append(`<input type="hidden" name="${key}" value="${val}">`);
+		form.prepend(`<input type="hidden" name="${key}" value="${val}">`);
 	}
 
 	form.on('submit', () => {
-		const product = params.has('product') ? ` · ${params.get('product')}` : '';
+		const product = params.has('product') ? (' · ' + params.get('product')) : '';
 		const message = form.find('[name="message"]').val().slice(0, 100);
 		const subject = `Feedback${product} - ${message}`;
-		form.append(`<input type="hidden" name="_subject" value="${subject}">`);
+		form.prepend(`<input type="hidden" name="_subject" value="${subject}">`);
+	});
+})();
+
+// For the imported navbar
+(() => {
+	// Burger menu toggle
+	$(document).on('click', '.nav-toggle', () => {
+		$('.nav-toggle, .nav-menu').toggleClass('is-active');
 	});
 })();
